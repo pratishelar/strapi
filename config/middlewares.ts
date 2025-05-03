0d6884aa79e94414b3db9d905e6810467bae2cbe
pratish <pshelar@conestogac.on.ca>
2025-05-03 14:47:11 -0400

    second

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,13 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:4200', 'https://your-angular.netlify.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
